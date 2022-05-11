54e71352cbf690083b85f0ce4cac51365893436a
Adam Ruthendorf-Przewoski <35307309+toiletgranny@users.noreply.github.com>
2022-05-11 22:34:07 +0200

    Added dark mode

--- js/_slider.js
+++ js/_slider.js
@@ -1,5 +1,9 @@
 export function update(item) {
   const sliderValue = (item.value-item.min)/(item.max-item.min)*100;
-  item.style.background = 'linear-gradient(to right, #205fdc 0%, #205fdc ' + sliderValue + '%, #0519691a ' + sliderValue + '%, #0519691a 100%)';
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const filledColor = '#205fdc';
+  const backgroundColor = isDark ? '#32343F' : '#E6E8F0';
+  // item.style.background = 'linear-gradient(to right, #205fdc 0%, #205fdc ' + sliderValue + '%, #0519691a ' + sliderValue + '%, #0519691a 100%)';
+  item.style.background = `linear-gradient(to right, ${filledColor} 0%, ${filledColor} ${sliderValue}%, ${backgroundColor} ${sliderValue}%, ${backgroundColor} 100%)`;
   item.nextElementSibling.childNodes[3].value = item.value
 }
